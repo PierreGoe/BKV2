320af40b8089b3d0d00b05ddafb3f4c432495a17
Pierre Goemans <p.goemans@hotmail.fr>
2021-11-11 13:58:15 +0100

    puppeter link

--- serveur/index.js
+++ serveur/index.js
@@ -1,12 +1,96 @@
 const express = require('express');
 const cors = require('cors');
+const puppeteer = require('puppeteer');
 
 const app = express();
 app.use(cors());
 
 app.get('/code', async (req, res) => {
-  const code = [{ value: 'bk99999' }, { value: 'bk99999' }];
-  res.send(code);
+  (async () => {
+    // Go to Final page of Form.
+    const browser = await puppeteer.launch({
+      headless: false,
+    });
+    const page = await browser.newPage();
+    await page.goto('https://www.bk-feedback-de.com/Index.aspx?c=051011');
+
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+    await page
+      .waitForSelector('#NextButton')
+      .then(() => page.click('#NextButton'));
+
+    // for (let index = 0; index < 21; index + 1) {
+    //   page.waitForSelector('#NextButton').then(() => page.click('#NextButton'));
+    // }
+
+    // Take result in Final page.
+    await page.waitForSelector('.ValCode');
+    const element = await page.$('.ValCode');
+    const elementProperty = await element.getProperty('innerHTML');
+    const innerHtml = await elementProperty.jsonValue();
+    const result = await innerHtml.replace('Validierungscode: ', '');
+    await browser.close();
+    await res.send([{ value: result }]);
+  })();
 });
 
 app.use('/', (req, res) => {
