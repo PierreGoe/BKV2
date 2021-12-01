185fc1f573c739d4915ef7fba100f76bfc93dc8f
Pierre Goemans <p.goemans@hotmail.fr>
2021-12-01 14:37:44 +0100

    add router

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GetCode from './GetCode';
+import Info from './Info';
 
 function App() {
-  return <GetCode />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<GetCode />} />
+        <Route path="/info" element={<Info />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
